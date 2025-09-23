ccb07688dc3d57605e4a02d3ae56627d125368a7
fer <20233005420@estudantes.ifpr.edu.br>
2025-09-23 15:00:43 -0300

    implementações aula (23/09)

--- todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
+++ todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
@@ -1,96 +1,66 @@
-// package br.edu.ifpr.todo.api.controller;
+package br.edu.ifpr.todo.api.controller;
 
-// import br.edu.ifpr.todo.api.dto.TarefaRequest;
-// import br.edu.ifpr.todo.api.dto.TarefaResponse;
-// import br.edu.ifpr.todo.domain.model.Tarefa;
-// import br.edu.ifpr.todo.domain.model.TodoStatus;
-// import br.edu.ifpr.todo.domain.service.TarefaService;
-// import jakarta.validation.Valid;
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.*;
-// import java.time.LocalDate;
-// import java.util.List;
+import br.edu.ifpr.todo.api.dto.TarefaRequest;
+import br.edu.ifpr.todo.api.dto.TarefaResponse;
+import br.edu.ifpr.todo.domain.model.Tarefa;
+import br.edu.ifpr.todo.domain.model.TodoStatus;
+import br.edu.ifpr.todo.domain.service.TarefaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-// @RestController
-// @RequestMapping("/tarefas")
-// // @CrossOrigin(origins = "*") // Libere se for consumir de um front rodando
-// em
-// // outra origem
-// public class TarefaController {
-// private final TarefaService service;
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/tarefa")
+public class TarefaController {
 
-// public TarefaController(TarefaService service) {
-// this.service = service;
-// }
-// // Exemplo: GET
-// // @RequestMapping("/tarefas")
-// // @CrossOrigin(origins = "*") // Libere se for consumir de um front rodando
-// em
+    private final TarefaService service;
 
-// // outra origem
-// public class TarefaController {
-// private final TarefaService service;
-// public TarefaController(TarefaService service) {
-// this.service = service;
-// }
-// // Exemplo: GET
-// //
-// http://localhost:8080/tarefas/add?nome=Teste&descricao=Primeira%20tarefa&impor
-// tante=true&status=FAZENDO&dataEntrega=2025-09-20
-// @GetMapping("/add")
-// @ResponseStatus(HttpStatus.CREATED)
-// public TarefaResponse criarViaGet(
-// @RequestParam String nome,
-// @RequestParam(required = false) String descricao,
-// @RequestParam(required = false, defaultValue = "A_FAZER")
-// TodoStatus status,
-// @RequestParam(required = false) Boolean importante,
-// @RequestParam(required = false) String dataEntrega) {
-// var dto = new TarefaRequest();
-// dto.setNome(nome);
-// dto.setDescricao(descricao);
-// dto.setStatus(status);
-// dto.setImportante(importante);
-// if (dataEntrega != null && !dataEntrega.isBlank()) {
-// dto.setDataEntrega(LocalDate.parse(dataEntrega)); // yyyy-MM-dd
-// }
-// Tarefa salvo = service.criar(dto);
-// return new TarefaResponse(
-// salvo.getId(),
-// salvo.getNome(),
-// salvo.getDescricao(),
-// salvo.getStatus(),
-// salvo.getDataCriacao(),
-// salvo.getDataEntrega(),
-// salvo.getImportante());
-// }
-// http://localhost:8080/tarefas/add?nome=Teste&descricao=Primeira%20tarefa&impor
-// tante=true&status=FAZENDO&dataEntrega=2025-09-20
-// @GetMapping("/add")
-// @ResponseStatus(HttpStatus.CREATED)
-// public TarefaResponse criarViaGet(
-// @RequestParam String nome,
-// @RequestParam(required = false) String descricao,
-// @RequestParam(required = false, defaultValue = "A_FAZER")
-// TodoStatus status,
-// @RequestParam(required = false) Boolean importante,
-// @RequestParam(required = false) String dataEntrega) {
-// var dto = new TarefaRequest();
-// dto.setNome(nome);
-// dto.setDescricao(descricao);
-// dto.setStatus(status);
-// dto.setImportante(importante);
-// if (dataEntrega != null && !dataEntrega.isBlank()) {
-// dto.setDataEntrega(LocalDate.parse(dataEntrega)); // yyyy-MM-dd
-// }
-// Tarefa salvo = service.criar(dto);
-// return new TarefaResponse(
-// salvo.getId(),
-// salvo.getNome(),
-// salvo.getDescricao(),
-// salvo.getStatus(),
-// salvo.getDataCriacao(),
-// salvo.getDataEntrega(),
-// salvo.getImportante());
-// }
-// }
+    public TarefaController(TarefaService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TarefaResponse criarViaPost(@RequestBody TarefaRequest dto) {
+        Tarefa salvo = service.criar(dto);
+
+        return new TarefaResponse(
+                salvo.getId(),
+                salvo.getNome(),
+                salvo.getDescricao(),
+                salvo.getStatus(),
+                salvo.getDataCriacao(),
+                salvo.getDataEntrega(),
+                salvo.getImportante());
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TarefaResponse> listarViaGet(
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String descricao,
+            @RequestParam(required = false, defaultValue = "A_FAZER") TodoStatus status,
+            @RequestParam(required = false) Boolean importante,
+            @RequestParam(required = false) LocalDate dataEntrega) {
+    
+        List<Tarefa> tarefas = service.listar(nome, status, importante, dataEntrega);
+    
+        List<TarefaResponse> resposta = new ArrayList<>();
+        for (Tarefa tarefa : tarefas) {
+            TarefaResponse dto = new TarefaResponse(
+                tarefa.getId(),
+                tarefa.getNome(),
+                tarefa.getDescricao(),
+                tarefa.getStatus(),
+                tarefa.getDataCriacao(),
+                tarefa.getDataEntrega(),
+                tarefa.getImportante()
+            );
+            resposta.add(dto);
+        }
+        return resposta;
+    }
+}    
