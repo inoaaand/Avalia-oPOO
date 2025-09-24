272ba85a57e6aca9a8cd15ac724e7fca1adbc6f1
Fernanda Silverio <20233005420@estudantes.ifpr.edu.br>
2025-09-24 15:55:32 -0300

    update controller

--- todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
+++ todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
@@ -1,15 +1,27 @@
 package br.edu.ifpr.todo.api.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.edu.ifpr.todo.api.dto.TarefaRequest;
 import br.edu.ifpr.todo.api.dto.TarefaResponse;
 import br.edu.ifpr.todo.domain.model.Tarefa;
 import br.edu.ifpr.todo.domain.model.TodoStatus;
 import br.edu.ifpr.todo.domain.service.TarefaService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @CrossOrigin("*")
@@ -45,22 +57,88 @@ public class TarefaController {
             @RequestParam(required = false, defaultValue = "A_FAZER") TodoStatus status,
             @RequestParam(required = false) Boolean importante,
             @RequestParam(required = false) LocalDate dataEntrega) {
-    
+
         List<Tarefa> tarefas = service.listar(nome, status, importante, dataEntrega);
-    
+
         List<TarefaResponse> resposta = new ArrayList<>();
         for (Tarefa tarefa : tarefas) {
             TarefaResponse dto = new TarefaResponse(
+                    tarefa.getId(),
+                    tarefa.getNome(),
+                    tarefa.getDescricao(),
+                    tarefa.getStatus(),
+                    tarefa.getDataCriacao(),
+                    tarefa.getDataEntrega(),
+                    tarefa.getImportante());
+            resposta.add(dto);
+        }
+        return resposta;
+    }
+
+    @GetMapping(params = "status")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TarefaResponse> filtrarPorStatus(@RequestParam TodoStatus status) {
+        List<Tarefa> tarefas = service.listar(null, status, null, null);
+        List<TarefaResponse> resposta = new ArrayList<>();
+        for (Tarefa tarefa : tarefas) {
+            TarefaResponse dto = new TarefaResponse(
+                    tarefa.getId(),
+                    tarefa.getNome(),
+                    tarefa.getDescricao(),
+                    tarefa.getStatus(),
+                    tarefa.getDataCriacao(),
+                    tarefa.getDataEntrega(),
+                    tarefa.getImportante());
+            resposta.add(dto);
+        }
+        return resposta;
+    }
+
+    @GetMapping(params = "importante")
+    @ResponseStatus(HttpStatus.OK)
+    public List<TarefaResponse> filtrarImportantes(@RequestParam Boolean importante) {
+        List<Tarefa> tarefas = service.listar(null, null, importante, null);
+        List<TarefaResponse> resposta = new ArrayList<>();
+        for (Tarefa tarefa : tarefas) {
+            TarefaResponse dto = new TarefaResponse(
+                    tarefa.getId(),
+                    tarefa.getNome(),
+                    tarefa.getDescricao(),
+                    tarefa.getStatus(),
+                    tarefa.getDataCriacao(),
+                    tarefa.getDataEntrega(),
+                    tarefa.getImportante());
+            resposta.add(dto);
+        }
+        return resposta;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TarefaResponse buscarPorId(@RequestParam Long id) {
+        Tarefa tarefa = service.buscarPorId(id);
+        return new TarefaResponse(
                 tarefa.getId(),
                 tarefa.getNome(),
                 tarefa.getDescricao(),
                 tarefa.getStatus(),
                 tarefa.getDataCriacao(),
                 tarefa.getDataEntrega(),
-                tarefa.getImportante()
-            );
-            resposta.add(dto);
-        }
-        return resposta;
+                tarefa.getImportante());
     }
-}    
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TarefaResponse atualizarParcial(@PathVariable Long id, @RequestBody TarefaRequest dto) {
+        Tarefa atualizado = service.atualizar(id, dto);
+        return new TarefaResponse(
+                atualizado.getId(), atualizado.getNome(), atualizado.getDescricao(), atualizado.getStatus(),
+                atualizado.getDataCriacao(), atualizado.getDataEntrega(), atualizado.getImportante());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void remover(@PathVariable Long id) {
+        service.remover(id);
+    }
+}
