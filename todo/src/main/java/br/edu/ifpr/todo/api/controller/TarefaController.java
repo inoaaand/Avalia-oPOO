fe2de31275ee6e9951f52a5fbb476018e0036d75
fer <20233005420@estudantes.ifpr.edu.br>
2025-09-29 07:49:39 -0300

    implementações finais

--- todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
+++ todo/src/main/java/br/edu/ifpr/todo/api/controller/TarefaController.java
@@ -3,20 +3,8 @@ package br.edu.ifpr.todo.api.controller;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import br.edu.ifpr.todo.api.dto.TarefaRequest;
 import br.edu.ifpr.todo.api.dto.TarefaResponse;
 import br.edu.ifpr.todo.domain.model.Tarefa;
@@ -115,7 +103,7 @@ public class TarefaController {
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public TarefaResponse buscarPorId(@RequestParam Long id) {
+    public TarefaResponse buscarPorId(@PathVariable Long id) {
         Tarefa tarefa = service.buscarPorId(id);
         return new TarefaResponse(
                 tarefa.getId(),
